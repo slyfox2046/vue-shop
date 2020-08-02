413e40598f08ed5d2b9b1508604d27ff73363759
slyfox <slyfox2046@gmail.com>
2020-08-02 20:54:31 +0800

    login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -4,12 +4,34 @@
       <div class="avatar_box">
         <img src="../assets/avatar2.png" alt="">
       </div>
+      <!-- 登陆表单区域 -->
+      <el-form ref="loginFormRef" :model="loginForm" :rules="loginFormRules" label-width="0px" class="login_form">
+        <el-form-item prop="username">
+          <el-input prefix-icon="iconfont icon-username" v-model="loginForm.username"></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input prefix-icon="iconfont icon-tuceng" v-model="loginForm.password" type="password"></el-input>
+        </el-form-item>
+        <el-form-item class="btns">
+          <el-button type="primary" @click="login">登录</el-button>
+          <el-button type="info" @click="resetLoginForm">重置</el-button>
+        </el-form-item>
+      </el-form>
     </div>
   </div>
 </template>
 <script>
   export default {
-
+    data() {
+      return {
+        loginForm: {},
+        loginFormRules: {}
+      }
+    },
+    methods: {
+      resetLoginForm() { },
+      login() { }
+    }
   }
 </script>
 <style lang="less" scoped>
@@ -34,11 +56,33 @@
     width: 130px;
     border: 1px solid #eee;
     border-radius: 50%;
+    padding: 10px;
+    box-shadow: 0 0 10px #ddd;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #fff;
 
     img {
       width: 100%;
       height: 100%;
+      border-radius: 50%;
+      background-color: #eee;
+
     }
 
+  }
+
+  .login_form {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    padding: 0 10px;
+    box-sizing: border-box;
+  }
+
+  .btns {
+    display: flex;
+    justify-content: flex-end;
   }
 </style>
