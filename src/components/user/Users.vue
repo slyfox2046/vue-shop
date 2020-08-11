12a16821c58b04d315824d1139391fa3f522f9ca
slyfox <slyfox2046@gmail.com>
2020-08-11 09:24:41 +0800

    user branch 修改验证手机号的规则

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -119,7 +119,7 @@
         cb(new Error('请输入合法的邮箱'))
       }
 
-      // 验证手机号德尔规则
+      // 验证手机号的规则
       var checkMobile = (rule, value, cb) => {
         const regMobile = /^(0|86|17951)?(13[0-9]|15[012356789]17[678]18[0-9]|14[57])[0-9]{8}$/
         if (regMobile.test(value)) {
